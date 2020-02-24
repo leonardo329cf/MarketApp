694fd35dbcc8b18dc6bb1b2063f9af3fac5b409a
leonardo329cf <leonardo329cf@gmail.com>
2020-02-24 11:47:32 -0300

    Criando Service e Repository para Categoria

--- src/main/java/com/leonardocardozo/marketapp/controllers/CategoriaController.java
+++ src/main/java/com/leonardocardozo/marketapp/controllers/CategoriaController.java
@@ -1,28 +1,28 @@
 package com.leonardocardozo.marketapp.controllers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonardocardozo.marketapp.entities.Categoria;
+import com.leonardocardozo.marketapp.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.addAll(Arrays.asList(cat1, cat2));
-		
-		return lista;		
+		return ResponseEntity.ok().body(cat);		
 	}
 }
