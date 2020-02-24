5b174a53a770447d2d23299b983b5dbcc5eabfe3
leonardo329cf <leonardo329cf@gmail.com>
2020-02-24 12:13:49 -0300

    Produto e associação muitos para muitos com Categoria

---
+++ src/main/java/com/leonardocardozo/marketapp/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.leonardocardozo.marketapp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonardocardozo.marketapp.entities.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
